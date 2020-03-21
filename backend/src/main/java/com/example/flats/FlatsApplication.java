248c2fbfbb9048639079522d170688bb4c067738
Artem Yakovenko <artem.yakovenko@avid.com>
2020-03-21 21:35:14 +0200

    [INIT] initial project setup

---
+++ backend/src/main/java/com/example/flats/FlatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.flats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/flats/FlatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
